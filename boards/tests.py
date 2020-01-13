21cc13aca068b6607b09e02f86578e646d3e1090
lichen <chen.li.a@17zuoye.com>
2020-01-13 18:31:22 +0800

    Initial commit

---
+++ boards/tests.py
@@ -0,0 +1,49 @@
+from django.urls import reverse, resolve
+from django.test import TestCase
+
+from .views import home, board_topics
+from .models import Board
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
+
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
+
+#
+# class BoardTopicTests(TestCase):
+#
+#     def test_board_topics_view_container_navigation_links(self):
+#         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+#         home_page_url = reverse('home')
+#         new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+#
+#         response = self.client.get(board_topics_url)
+#
+#         self.assertContains(response, 'href="{0}"'.format(home_page_url))
+#         self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
